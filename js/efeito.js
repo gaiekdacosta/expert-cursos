105f1dc4c57d7ebbddf515c01eaea542ebfbd854
gaiek da costa <pelipe.gaiek1701@gmail.com>
2024-06-26 23:11:12 -0300

    changes in courses and add about us

--- js/efeito.js
+++ js/efeito.js
@@ -34,3 +34,60 @@ efeitorevelar.reveal('.efeito-reveal-card4', {
     distance: '90px',
     delay: 1500
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.querySelector('.cursos-container');
+    
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+    let isDragging = false;
+    let velocity = 0;
+    let rafId;
+
+    container.addEventListener('mousedown', (e) => {
+        isDown = true;
+        isDragging = false;
+        startX = e.pageX - container.offsetLeft;
+        scrollLeft = container.scrollLeft;
+        cancelMomentumTracking();
+    });
+
+    container.addEventListener('mouseleave', () => {
+        isDown = false;
+    });
+
+    container.addEventListener('mouseup', () => {
+        isDown = false;
+        if (isDragging) {
+            startMomentumTracking();
+        }
+    });
+
+    container.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        isDragging = true;
+        const x = e.pageX - container.offsetLeft;
+        const walk = (x - startX) * 0.5; // Reduzir a quantidade de movimento por vez
+        container.scrollLeft = scrollLeft - walk;
+        velocity = walk;
+    });
+
+    function startMomentumTracking() {
+        cancelMomentumTracking();
+        rafId = requestAnimationFrame(momentumLoop);
+    }
+
+    function cancelMomentumTracking() {
+        cancelAnimationFrame(rafId);
+    }
+
+    function momentumLoop() {
+        container.scrollLeft -= velocity;
+        velocity *= 0.95; 
+        if (Math.abs(velocity) > 0.5) {
+            rafId = requestAnimationFrame(momentumLoop);
+        }
+    }
+});
